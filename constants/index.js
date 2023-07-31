42005ea3803ee1ff25dae7f3c5d52a72df2554a6
Hamidreza7575 <111297271+Hamidreza7575@users.noreply.github.com>
2023-07-31 02:16:15 +0000

    Update index.js
    Update Contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x7dccd7ddd37068bb6b86c0aab7eeaa273b8d18ca";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
